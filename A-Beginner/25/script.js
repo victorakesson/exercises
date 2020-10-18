7ebdc06ea9e0bc4bda41dbe423184ff83899570e
victorakesson <65647015+victorakesson@users.noreply.github.com>
2020-10-18 15:31:51 +0200

    Uppgift 25

---
+++ A-Beginner/25/script.js
@@ -0,0 +1,11 @@
+function isLagom(x){
+    if ( x > 16 ) {
+        return false 
+     } else if ( x < 8 ) {
+            return true
+        } else { 
+            return true
+        }
+
+        }
+    
